c06fe5a87c3339e499556087d3655ae8034898ce
Mariana Diaz <marianadiazcastellanos@gmail.com>
2023-05-22 19:07:12 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+
+//OBTENIENDO EL VALUE DE LA CLASE RANGE
+// var range = $('.input-range'),
+//     value = $('.range-value');
+    
+// value.html(range.attr('value'));
+
+// range.on('input', function(){
+//     value.html(this.value);
+// }); 
+
+//agregar un listener del evento.
+// funcion => tomar el valor del height -> cambiar el background 
+
+//volver a subir a github ! 
